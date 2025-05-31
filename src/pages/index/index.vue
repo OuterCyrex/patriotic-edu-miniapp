01409f5b5561d4dcade1d21fc5ab0ed40f7bc21f
OuterCyrex <a15839962056@outlook.com>
2025-05-31 12:06:22 +0800

    🎉 feat: init the front project

---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <AtButton>{{msg}}</AtButton>
+  <AtButton type='primary'>按钮文案</AtButton>
+  <AtButton type='secondary'>按钮文案</AtButton>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
